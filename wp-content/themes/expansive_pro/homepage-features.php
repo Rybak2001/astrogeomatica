5a19242ad770a83bfe4ccc15e9bc3d2245be0584
Rybak2001 <139817600+Rybak2001@users.noreply.github.com>
2024-03-06 18:12:58 -0400

    sdsadasdasdasdsadasdas

--- wp-content/themes/expansive_pro/homepage-features.php
+++
@@ -1,8 +0,0 @@
-<?php $query = new WP_Query('post_type=cpo_feature&posts_per_page=-1&order=ASC&orderby=menu_order'); ?>
-<?php if($query->posts): $feature_count = 0; ?>
-<div id="features" class="features">
-	<?php //cpotheme_block('home_features', 'features-heading section-heading'); ?>
-	<?php $columns = cpotheme_get_option('features_columns'); if($columns == '') $columns = 2; ?>
-	<?php cpotheme_grid($query->posts, 'element', 'feature', $columns, array('class' => 'column-early column-fit')); ?>
-</div>
-<?php endif; ?>
